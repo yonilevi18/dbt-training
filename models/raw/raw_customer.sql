8d80e04d3ee0bc62c904278abe7b31fe35593f1f
leviyoni5@gmail.com <leviyoni5@gmail.com>
2023-02-01 14:56:53 +0000

    developed raw_orders, raw_customer, raw_product

---
+++ models/raw/raw_customer.sql
@@ -0,0 +1,8 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+
+select * from raw.globalmart.customer
---
+++ models/raw/raw_product.sql
@@ -0,0 +1,8 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+
+Select * from raw.globalmart.product
